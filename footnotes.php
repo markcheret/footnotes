6ff77dc74d764697ed48f414ed370096d1327f97
pewgeuges <73141620+pewgeuges@users.noreply.github.com>
2021-03-05 08:58:48 +0100

    Also added @‌revision and @‌timestamp PHPDOC tags @‌revision and @‌timestamp used for SVN refs.
    Full enumeration of added custom PHPDOC tags:
    @‌accountable, @‌bib, @‌biblio, @‌callback, @‌commit, @‌committer, @‌contributor, @‌courtesy, @‌date, @‌datetime, @‌lastmodified, @‌modified, @‌publisher, @‌reporter, @‌revision, @‌timestamp, @‌user.Some tags like @‌reporter and @‌contributor are more used than others.

--- footnotes.php
+++ footnotes.php
@@ -44,8 +44,8 @@ define( 'C_STR_PACKAGE_VERSION', '2.5.11d1' );
  * - Bugfix: Codebase: revert to 2.5.8, thanks to @little-shiva @watershare @adjayabdg @staho @frav8 @voregnev @dsl225 @alexclassroom @a223123131 @codldmac bug reports.
  * 
  * @version 2.5.10 (reversion to @version 2.5.8)
- * @date 2021-03-01 11:09:29 +0000
  * @revision 2483464
+ * @timestamp 2021-03-01 11:09:29 +0000
  * @link https://plugins.trac.wordpress.org/changeset/2483464/footnotes/trunk
  * 
  * @reporter @little-shiva

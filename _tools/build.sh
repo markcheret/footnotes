2bd9079512824fa6be26c722a589bed89004b3cd
Rumperuu <me+footnotes@bengoldsworthy.net>
2021-03-11 10:39:50 +0000

    Refactor script

--- _tools/build.sh
+++ _tools/build.sh
@@ -1,68 +1,84 @@
 #!/bin/bash
-# 2021-02-15T1839+0100
-# Last modified: 2021-02-16T0155+0100
-# csscat.sh
-# Concatenates CSS files; deletes the concatenations after they’ve been minified.
+
+# build-stylesheets.sh
+# Concatenates, minifies (TODO) and deploys stylesheets for distribution.
 #
-# 12 unified style sheets are concatenated out of these files:
-# dev-common.css
-# dev-tooltips.css
-# dev-tooltips-alternative.css
-# dev-layout-reference-container.css
-# dev-layout-entry-content.css
-# dev-layout-main-content.css
-#
-echo "Running $(dirname "$0")/csscat.sh"
-if [[ $1 == "c" ]]; then
-echo "Concatenate in csscat/ for minification:"
-cd ../css
-mkdir csscat
-cat dev-common.css > csscat/footnotes-nottbrpl0.css
-cat dev-common.css dev-layout-reference-container.css > csscat/footnotes-nottbrpl1.css
-cat dev-common.css dev-layout-entry-content.css > csscat/footnotes-nottbrpl2.css
-cat dev-common.css dev-layout-main-content.css > csscat/footnotes-nottbrpl3.css
-cat dev-common.css dev-tooltips.css > csscat/footnotes-jqttbrpl0.css
-cat dev-common.css dev-tooltips.css dev-layout-reference-container.css > csscat/footnotes-jqttbrpl1.css
-cat dev-common.css dev-tooltips.css dev-layout-entry-content.css > csscat/footnotes-jqttbrpl2.css
-cat dev-common.css dev-tooltips.css dev-layout-main-content.css > csscat/footnotes-jqttbrpl3.css
-cat dev-common.css dev-tooltips.css dev-tooltips-alternative.css > csscat/footnotes-alttbrpl0.css
-cat dev-common.css dev-tooltips.css dev-tooltips-alternative.css dev-layout-reference-container.css > csscat/footnotes-alttbrpl1.css
-cat dev-common.css dev-tooltips.css dev-tooltips-alternative.css dev-layout-entry-content.css > csscat/footnotes-alttbrpl2.css
-cat dev-common.css dev-tooltips.css dev-tooltips-alternative.css dev-layout-main-content.css > csscat/footnotes-alttbrpl3.css
-echo "Done."
-elif [[ $1 == "d" ]]; then
-echo "Move minified to css/ and delete concatenations and their temp dir:"
-cd ../css/csscat
-mv footnotes-nottbrpl0.min.css ..
-mv footnotes-nottbrpl1.min.css ..
-mv footnotes-nottbrpl2.min.css ..
-mv footnotes-nottbrpl3.min.css ..
-mv footnotes-jqttbrpl0.min.css ..
-mv footnotes-jqttbrpl1.min.css ..
-mv footnotes-jqttbrpl2.min.css ..
-mv footnotes-jqttbrpl3.min.css ..
-mv footnotes-alttbrpl0.min.css ..
-mv footnotes-alttbrpl1.min.css ..
-mv footnotes-alttbrpl2.min.css ..
-mv footnotes-alttbrpl3.min.css ..
-cd ..
-rm -r csscat
-echo "Done."
+# 12 unified stylesheets are concatenated out of these files:
+# - `dev-common.css`
+# - `dev-tooltips.css`
+# - `dev-tooltips-alternative.css`
+# - `dev-layout-reference-container.css`
+# - `dev-layout-entry-content.css`
+# - `dev-layout-main-content.css`
+
+echo "Running $(dirname "$0")/build-stylesheets.sh"
+
+if [[ $1 == "-c" ]]; then
+
+	echo "Concatenating files and placing in \`css/tmp/\`."
+	mkdir -p ./css/tmp
+	cat ./css/dev-common.css > ./css/tmp/footnotes-nottbrpl0.css
+	cat ./css/dev-{common,layout-reference-container}.css > ./css/tmp/footnotes-nottbrpl1.css
+	cat ./css/dev-{common,layout-entry-content}.css > ./css/tmp/footnotes-nottbrpl2.css
+	cat ./css/dev-{common,layout-main-content}.css > ./css/tmp/footnotes-nottbrpl3.css
+	cat ./css/dev-{common,tooltips}.css > ./css/tmp/footnotes-jqttbrpl0.css
+	cat ./css/dev-{common,tooltips,layout-reference-container}.css > ./css/tmp/footnotes-jqttbrpl1.css
+	cat ./css/dev-{common,tooltips,layout-entry-content}.css > ./css/tmp/footnotes-jqttbrpl2.css
+	cat ./css/dev-{common,tooltips,layout-main-content}.css > ./css/tmp/footnotes-jqttbrpl3.css
+	cat ./css/dev-{common,tooltips,tooltips-alternative}.css > ./css/tmp/footnotes-alttbrpl0.css
+	cat ./css/dev-{common,tooltips,tooltips-alternative,layout-reference-container}.css > ./css/tmp/footnotes-alttbrpl1.css
+	cat ./css/dev-{common,tooltips,tooltips-alternative,layout-entry-content}.css > ./css/tmp/footnotes-alttbrpl2.css
+	cat ./css/dev-{common,tooltips,tooltips-alternative,layout-main-content}.css > ./css/tmp/footnotes-alttbrpl3.css
+	cat ./css/settings.css > ./css/tmp/settings.css
+	echo "Done."
+
+elif [[ $1 == "-m" ]]; then
+
+	# TODO: this should automatically minifiy all files, outputting into `.min.css`
+	# files and deleting the original concatenated `.css` files in `css/tmp/`.
+	# Once that's done, we can change the `rm -r` command in the deploy step to
+	# `rmdir`, which will throw us an error if we have any minified files that
+	# haven't been moved over to `dist/css/` for whatever reason. As it currently
+	# stands, we have no error checking in place.
+	echo "Minifying files (TODO)..."
+	mkdir -p ./dist/css
+	for f in ./css/tmp/*.css;	do
+		filename=$(basename $f .css)
+		echo $filename
+		# TODO: automated minification
+		echo $filename Done
+	done
+	echo "Done."
+	
+elif [[ $1 == "-d" ]]; then
+
+	echo "Deploying minified files to \`dist/css/\`..."
+	mkdir -p ./dist/css
+	for f in ./css/tmp/*.min.css;	do
+		filename=$(basename $f .css)
+		echo Moving $filename
+		mv $f ./dist/css
+		echo $filename Moved
+	done
+	echo "Deleting temporary files..."
+	rm -r ./css/tmp
+	echo "Done."
+	
 else
-echo "Concatenates CSS files; deletes the concatenations after they’ve been minified."
-echo ""
-echo "12 unified style sheets are concatenated out of these files:"
-echo ""
-echo "dev-common.css"
-echo ""
-echo "dev-tooltips.css"
-echo "dev-tooltips-alternative.css"
-echo ""
-echo "dev-layout-reference-container.css"
-echo "dev-layout-entry-content.css"
-echo "dev-layout-main-content.css"
-echo ""
-echo "Command: c: Concatenate temporary files waiting for minification."
-echo "Command: d: Move the minified files to css/, delete the temporary."
-echo "No command, \"--help\", or anything else: Output this help section."
+
+	echo "Concatenates, minifies (TODO) and deploys stylesheets for distribution.\n"
+	
+	echo "12 unified style sheets are concatenated out of these files:\n"
+	echo "\`dev-common.css\`"
+	echo "\`dev-tooltips.css\`"
+	echo "\`dev-tooltips-alternative.css\`"
+	echo "\`dev-layout-reference-container.css\`"
+	echo "\`dev-layout-entry-content.css\`"
+	echo "\`dev-layout-main-content.css\`\n"
+	
+	echo "Command: \`-c\`: Concatenate \`dev-*\` CSS files into temporary directory."
+	echo "Command: \`-m\`: Minify files (TODO)."
+	echo "Command: \`-d\`: Deploy minified files to \`dist/css/\` and remove temporary files."
+	echo "No command, \"--help\", or anything else: Output this help section."
+	
 fi

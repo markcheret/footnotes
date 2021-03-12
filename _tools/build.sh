e8d6dbe13d81b5c9fbb0cfaa49cbce3907dfc43b
Rumperuu <me+footnotes@bengoldsworthy.net>
2021-03-12 17:53:49 +0000

    Automatically generate new releases (#59)
    * Adapt after the 2.5.9d1 accident.
    
    * Corrections to changelog.
    
    * Also added @‌revision and @‌timestamp PHPDOC tags
    @‌revision and @‌timestamp used for SVN refs.
    
    Full enumeration of added custom PHPDOC tags:
    @‌accountable, @‌bib, @‌biblio, @‌callback, @‌commit, @‌committer, @‌contributor, @‌courtesy, @‌date, @‌datetime, @‌lastmodified, @‌modified, @‌publisher, @‌reporter, @‌revision, @‌timestamp, @‌user.Some tags like @‌reporter and @‌contributor are more used than others.
    
    * Readme header upgrade.
    
    * Create deploy-to-wordpress.org.yml
    
    * Add distignore
    
    * Comment out `build` command, replace npm with composer
    
    * Remove placeholder comments
    
    * Add build commands
    
    * Fix typo
    
    * Fix version number bug
    
    * Make zip without top-level directory
    
    * Append tag name to plugin zip
    
    * Clean up a bit
    
    * Rename workflow config
    
    * Fix upload paths
    
    * Append tag name to artifact
    
    * Fix paths
    
    * Revert path fix
    
    * Try again
    
    * Add wplm-config.xml to buildfiles
    
    * Fix indentation
    
    * Rename development/csscat.sh to css/csscat.sh
    
    * Rename development/customized-documentation-schema.txt to customized-documentation-schema.txt
    
    * Rename development/customized-template-stack.txt to customized-template-stack.txt
    
    * Version number related fixes before pull request.
    
    * Move csscat script to tools dir
    
    * Rename csscat
    
    * Refactor script
    
    * Update customized-documentation-schema.txt
    
    * Replace empty lines in help output
    
    * Escape empty lines.
    
    * Delete 3 items from `.distignore`
    As mentioned, CONTRIBUTING.md and README.md should be included in distributions.
    
    - As an invitation to the Community.
    - As a tangible proof of goodwill after the 2.5.9d1 accident.
    
    Also SECURITY.md so Footnotes users see that we’re concerned, and
    can reach out without transiting via GitHub.
    
    * Include CONTRIBUTING.md README.md SECURITY.md
    
    * Update sync w/ 5.7 tested.
    
    * Update
    
    * Update composerfiles
    
    Co-authored-by: pewgeuges <73141620+pewgeuges@users.noreply.github.com>

---
+++ _tools/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Moves everything except the stylesheets over to `dist/`
+
+cp -r -t dist class/ js/ img/ languages/ templates/
+cp -t dist features.txt license.txt readme.txt footnotes.php includes.php wpml-config.xml customized-documentation-schema.txt customized-template-stack.txt CONTRIBUTING.md README.md SECURITY.md
+# TODO: once automatic minification is implemented, this should handle that.
+# For now, we shall have to assume that this command is being run on a repo. with
+# minimised stylesheet files already in `dist/css/`.

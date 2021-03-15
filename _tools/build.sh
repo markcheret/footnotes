edadb32a27e9bffe5d86bfb4d23d0c5784946d42
Rumperuu <me+footnotes@bengoldsworthy.net>
2021-03-15 11:00:56 +0000

    Add error-checking, stylesheet minimisation

--- _tools/build.sh
+++ _tools/build.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
-# Moves everything except the stylesheets over to `dist/`
+echo "Building Plugin..."
 
-cp -r -t dist class/ js/ img/ languages/ templates/
-cp -t dist features.txt license.txt readme.txt footnotes.php includes.php wpml-config.xml customized-documentation-schema.txt customized-template-stack.txt CONTRIBUTING.md README.md SECURITY.md
 # TODO: once automatic minification is implemented, this should handle that.
 # For now, we shall have to assume that this command is being run on a repo. with
 # minimised stylesheet files already in `dist/css/`.
+echo "Building stylesheets..."
+./_tools/build-stylesheets.sh -c
+if [[ $? != 0 ]]; then echo "Concatenation failed!"; exit 1; fi
+./_tools/build-stylesheets.sh -m
+if [[ $? != 0 ]]; then echo "Minification failed!"; exit 1; fi
+./_tools/build-stylesheets.sh -d
+if [[ $? != 0 ]]; then echo "Deployment failed!"; exit 1; fi
+echo "Stylesheet build complete."
+
+# Moves everything else over to `dist/`
+echo "Copying directories..."
+cp -r -t dist class/ js/ img/ languages/ templates/
+echo "Copying files..."
+cp -t dist features.txt license.txt readme.txt footnotes.php includes.php wpml-config.xml customized-documentation-schema.txt customized-template-stack.txt CONTRIBUTING.md README.md SECURITY.md
+
+echo "Build complete."
+exit 0

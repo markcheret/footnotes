9d90689165f2b37a7964def15f0b9f02f27c6c30
Rumperuu <me+footnotes@bengoldsworthy.net>
2021-04-15 15:29:07 +0100

    fix: lint JS line used for version parsing

--- js/wysiwyg-editor.js
+++ js/wysiwyg-editor.js
@@ -68,7 +68,7 @@
 				author : 'Mark Cheret',
 				authorurl : 'https://cheret.org/footnotes/',
 				infourl : 'https://wordpress.org/plugins/footnotes/',
-				version : '2.6.5'
+				version: '2.6.5'
 			};
 		}
 	});

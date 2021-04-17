3d85de134e877c27355ae49bda972f86cc5742ed
Mark Cheret <mark@cheret.de>
2021-04-17 23:32:43 +0200

    Update task.php
    attribution

--- class/task.php
+++ class/task.php
@@ -2094,6 +2094,8 @@ class MCI_Footnotes_Task {
 				// Determine tooltip content.
 				if ( MCI_Footnotes::$a_bool_tooltips_enabled ) {
 					$l_str_tooltip_content = $l_bool_has_tooltip_text ? $l_str_tooltip_text : $l_str_excerpt_text;
+					// @reporter @pewgeuges
+					// @link https://github.com/markcheret/footnotes/issues/103
 					$l_arr_paragraph_splitters = array( '#(</p *>|<p[^>]*>)#', '#(</div *>|<div[^>]*>)#' );
 					$l_str_tooltip_content = preg_replace( $l_arr_paragraph_splitters, '<br />', $l_str_tooltip_content );
 				} else {

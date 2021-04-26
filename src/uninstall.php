871d05f929de29d280dd7e0ca29b240458382a9d
Rumperuu <me+git@bengoldsworthy.net>
2021-04-26 22:53:51 +0100

    chore: add placeholder uninstall file

---
+++ src/uninstall.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * When populating this file, consider the following flow
+ * of control:
+ *
+ * - This method should be static
+ * - Check if the `$_REQUEST` content actually is the plugin name
+ * - Run an admin referrer check to make sure it goes through authentication
+ * - Verify the output of `$_GET` makes sense
+ * - Repeat with other user roles. Best directly by using the links/query string parameters.
+ * - Repeat things for multisite. Once for a single site in the network, once sitewide.
+ *
+ * @since      2.8.0
+ *
+ * @package    footnotes
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}

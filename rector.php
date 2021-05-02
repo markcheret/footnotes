7d72930899cc96675a31f9e182cad207eed886cf
Rumperuu <me+git@bengoldsworthy.net>
2021-05-02 21:24:59 +0100

    ci: update Rector config

--- rector.php
+++ rector.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\DowngradeSetList;
 use Rector\Core\ValueObject\PhpVersion;
+use Rector\Set\ValueObject\SetList;
+//use Rector\Set\ValueObject\DowngradeSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -16,16 +16,16 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/src',
     ]);
 
+    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+
     // Define what rule sets will be applied
     $parameters->set(Option::SETS, [
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::TYPE_DECLARATION,
+        // TODO:
+        //SetList::PRIVATIZATION
+        // TODO: in Build
        // DowngradeSetList::PHP_80,
        // DowngradeSetList::PHP_74,
        // DowngradeSetList::PHP_73,
         //DowngradeSetList::PHP_72,
     ]);
-
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_71);
 };
